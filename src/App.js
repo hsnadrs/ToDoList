d2b126596cadbe92c4540b4d057d7e1077b6faf1
hsnadrs <hsnadrs@yahoo.com>
2023-01-05 14:52:59 +0000

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  const [favourites, setFavourites] = useState([]);
+  //let toDoList =["aaa","bbb","ccc"];
+  const [toDoList, setToDoList]= useState (["aaa","bbb","ccc"]);
+  const[newToDo, setNewToDo] = useState(""); 
+
+  const handleAddToDo = () => {
+   
+    let tempToDo = [...toDoList];
+    tempToDo.push(newToDo);
+    setToDoList(tempToDo);
+  } 
+
+  const handleRemoveToDo = (index) => {
+    let tempToDo = [...toDoList];
+    tempToDo.splice(index, 1);
+    setToDoList(tempToDo);
+  } 
+// const handleEntry =(event)=>{
+//   newToDo += event.target.value;
+
+// }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        
+        <div id="addBox">
+      
+
+      <h3>ALL To Do</h3>
+      {toDoList.map((toDoList, index) => {
+        return <HeroCard key={index} heroObj={toDoList}  removeToDo={handleRemoveToDo}/>
+      })}
+{/* <input type="text" onChange={handleEntry} className="newToDoEntry" value="">type to Do</input> */}
+<p><input  className="txtBox"  value={newToDo} onChange={(e) => {setNewToDo(e.target.value)}} /> </p>
+{/* <button onClick={handle}>Button</button> */}
+<p><button   onClick={handleAddToDo}>Add new to Do</button></p>
+</div>
+{/* box */}
     </div>
+    // main
   );
+};
+
+const Favourites = ({ favHeroData, removeToDo }) => {
+  return (
+    <div>
+      <p>{favHeroData.hero}</p>
+      <button onClick={removeToDo}>X</button>
+    </div>
+  )
+}
+
+const HeroCard = (props) => {
+  const [done, setdone] = useState(true);
+  //let index = props.key;
+  return (
+    <>
+    
+      
+
+      {done && (
+        <div>
+          
+          <p>To Do: {props.heroObj}</p>
+        </div>
+      )}   
+        {!done && (
+        <div>
+          
+          <p><s>To Do: {props.heroObj}</s></p>
+        </div>
+      )}   
+
+      <button onClick={() => setdone(!done)}>{ done ? "not done INFO" : "done INFO" }</button>
+      
+      <button onClick={props.removeToDo}>X</button>
+    </>
+  )
 }
 
 export default App;
